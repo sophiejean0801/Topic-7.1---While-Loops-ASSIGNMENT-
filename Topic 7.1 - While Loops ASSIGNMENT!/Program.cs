c4a597fa51360a7553a9fccf3d80a1de1763eae2
Sophie Jean <363449299@GoTVDSB.ca>
2025-03-20 14:23:09 -0400

    Add project files.

---
+++ Topic 7.1 - While Loops ASSIGNMENT!/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace Topic_7._1___While_Loops_ASSIGNMENT_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Red;
+            bool done = false;
+            string guess;
+            int flip, points = 3, heads = 0, tails = 1;
+            Random random = new Random();
+
+            // heads is 0 and tails is 1
+            
+
+            Console.WriteLine("Welcome to the worst casino! You will be playing a game of chance. ");
+            while (!done)
+            {
+                Console.WriteLine("Enter heads or tails");
+                guess = Console.ReadLine();
+                flip = random.Next(0, 2);
+
+                if (guess == "heads" && flip == 0)
+                {
+                    Console.WriteLine("The coin landed on heads! You win!");
+                    Console.WriteLine("You now have " + points + " points!");
+                }
+                if (guess == "tails" && flip == 1)
+                {
+                    Console.WriteLine("The coin landed on tails! You win!");
+                    Console.WriteLine("You now have " + points + " points!");
+                }
+                if (guess == "heads" && flip == 1)
+                {
+                    Console.WriteLine("You guessed heads and the coin landed on tails! You suck!");
+                    points--;
+                    Console.WriteLine("You now have " + points + " points!");
+                }
+                if (guess == "tails" && flip == 0)
+                {
+                    Console.WriteLine("You guessed tails and the coin landed on heads! You suck!");
+                    points--;
+                    Console.WriteLine("You now have " + points + " points!");
+                }
+                if (guess != "heads" && guess != "tails")
+                {
+                    points--;
+                    Console.WriteLine("The coin somehow landed on its side. You still lose a point though. You have " + points + " points remaining.");
+                }
+                if (points == 0)
+                {
+                    Console.WriteLine("You have no points left. You loser. See you never!");
+                    done = true;
+                }
+                Console.WriteLine("Would you like to continue?");
+                if (Console.ReadLine() == "no")
+                {
+                    Console.WriteLine("Coward.");
+                    done = true;
+                }
+                else
+                {
+                    done = false;
+                }
+                while (done == true)
+                {
+                    Console.WriteLine("You have " + points + " points. Would you like to play again?");
+                    if (points == 0)
+                    {
+                        done = true;
+                        Console.WriteLine("You have no points left. You loser. See you never!");
+                    }
+                    if (Console.ReadLine() == "yes")
+                    {
+                        done = false;
+                        points = 3;
+                        Console.WriteLine("Welcome back to the worst casino! You will be playing a game of chance. ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Coward. You have " + points + " points. See you never!");
+                        done = true;
+                    }
+                }
+
+
+
+
+
+
+
+        }   }    
+    }
+}
